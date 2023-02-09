208706713d1738eaa363844bd04c0813f480ca3b
EliezerSA <eliezersantiago-100@outlook.com>
2023-02-08 19:37:34 -0600

    Nombre del Proyecto: my-app. Descripcion: Version final del proyecto my-app

---
+++ src/main/java/com/eli/myapp/entity/TestEntity.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.eli.myapp.entity;
+
+/**
+ * @author Eliezer Santiago Fuentes
+ *
+ */
+public class TestEntity {
+	/*
+	 * Propiedad de prueba para la muestra de compilacion del archivo testentity
+	 * 
+	 * */
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String nombre;
+	
+
+}
